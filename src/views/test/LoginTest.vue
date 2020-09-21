03a76e037356cabea0633e1fe5c1f3baa90ade83
ZHICHENGH <48426997+ZHICHENGH@users.noreply.github.com>
2020-09-21 23:38:05 +1000

    add rules to input and write api js

--- src/views/test/LoginTest.vue
+++ src/views/test/LoginTest.vue
@@ -1,7 +1,7 @@
 <template>
 <v-container class="mt-16 max-width=240 "
       justify="center">
-  <v-form ref="form" v-model="valid" :lazy-validation="lazy" @submit.prevent="login" >
+  <v-form ref="form" v-model="valid"  lazy-validation @submit.prevent="login" >
     <v-row
       class="mb-1"
       no-gutters
@@ -13,8 +13,7 @@
             label="username"
             placeholder="username"
             v-model="username"
-            required
-            outlined
+            :rules="[rules.required, rules.max,rules.min1]"
           ></v-text-field>
       </v-col>
       <v-col cols="12" sm="4">
@@ -31,8 +30,10 @@
             label="password"
             placeholder="password"
             v-model="password"
-            required
-            outlined
+            :rules="[rules.min,rules.max,rules.required]"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            @click:append="show1 = !show1"
           ></v-text-field>
       </v-col>
       <v-col cols="12" sm="4">
@@ -45,7 +46,7 @@
     <v-col cols="12" sm="4">
       </v-col>
     <v-col cols="12" sm="4">
-          <v-btn class = "ml-7" @click="login">Login</v-btn> 
+          <v-btn class = "ml-7" :disabled="!valid" @click="login">Login</v-btn> 
           <v-btn class = "ml-7" @click="logout">Logout</v-btn> 
       </v-col>
       <v-col cols="12" sm="4">
@@ -61,10 +62,16 @@
 export default {
   name: 'LoginTest',
   data: () => ({
-    lazy: true,
     valid: false,
     username: '',
     password: '',
+    show1:false,
+    rules:{
+          required: v=> !!v || 'Required.',
+          min: v => v.length>=8 || 'Min 8 characters',
+          min1: v => v.length>=3 || 'Min 3 characters',
+          max: v => v.length<=20 || 'Max 20 characters',
+      }
   }),
   props: {
     info: {
@@ -74,20 +81,24 @@ export default {
   },
   methods: {
     async login() {
+      if(this.$refs.form.validate()){
         let student = {
           username: this.username, 
           password: this.password
         }
         const [res, success]  = await this.$request.post("/api/student/login", student).catch(err=>
-          console.log(err)
+        console.log(err)
         )
         if (success) {
           this.info = 'success'
           this.$router.push('loginInfo')
+          alert("success!")
         }
         else {
           this.info = res.error.message
+          alert("failed")
         }
+      }
     },
     async logout() {
         this.$store.dispatch('logout')
