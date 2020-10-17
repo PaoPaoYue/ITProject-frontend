cc354db38dbd9a38939d4d1d1a768d4b19615ae5
ZHICHENGH <zhichengh@student.unimelb.edu.au>
2020-10-18 02:30:15 +1100

    add ckeditor5 to EditContent

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
+const path = require( 'path' );
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+
 module.exports = {
   "transpileDependencies": [
-    "vuetify"
+    "vuetify",
+    /ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/,
   ],
   devServer: {
     port: 5110,
@@ -15,5 +20,35 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+        new CKEditorWebpackPlugin( {
+            language: 'en',
+            translationsOutputFile: /app/
+        } )
+    ]
+  },
+chainWebpack: config => {
+  const svgRule = config.module.rule( 'svg' );
+  svgRule.exclude.add( path.join( __dirname, 'node_modules', '@ckeditor' ) );
+  config.module
+      .rule( 'cke-svg' )
+      .test( /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/ )
+      .use( 'raw-loader' )
+      .loader( 'raw-loader' );
+  config.module
+      .rule( 'cke-css' )
+      .test( /ckeditor5-[^/\\]+[/\\].+\.css$/ )
+      .use( 'postcss-loader' )
+      .loader( 'postcss-loader' )
+      .tap( () => {
+          return styles.getPostCssConfig( {
+              themeImporter: {
+                  themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
+              },
+              minify: true
+          } );
+      } );
   }
 }
