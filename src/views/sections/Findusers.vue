c6eb78e33ecf2eb4d5ab623464489986c660213a
Tian Qiu <tq@student.unimelb.edu.au>
2020-10-17 20:47:16 +0800

    change explore style divider

--- src/views/sections/Findusers.vue
+++ src/views/sections/Findusers.vue
@@ -29,46 +29,53 @@
                 />
               </v-form>
 
-              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
-                <v-container class="py-2 pl-0 pr-0">
-                  <v-card
-                    class="mx-auto"
-                    max-width="1200"
-                    outlined
-                  >
-                    <v-container class="py-2 pl-5">
-                      <news-author v-bind="author" />
-                    </v-container>
-                  </v-card>
-                </v-container>
-              </router-link>
-              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
-                <v-container class="py-2 pl-0 pr-0">
-                  <v-card
-                    class="mx-auto"
-                    max-width="1200"
-                    outlined
-                  >
-                    <v-container class="py-2 pl-5">
-                      <news-author v-bind="author" />
-                    </v-container>
-                  </v-card>
-                </v-container>
-              </router-link>
-              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
-                <v-container class="py-2 pl-0 pr-0">
-                  <v-card
-                    class="mx-auto"
-                    max-width="1200"
-                    outlined
-                  >
-                    <v-container class="py-2 pl-5">
-                      <news-author v-bind="author" />
-                    </v-container>
-                  </v-card>
-                </v-container>
-              </router-link>
 
+              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
+                <v-container class="py-2 pl-0 pr-0">
+                  <v-card
+                    elevation="0"
+                    class="mx-auto"
+                    max-width="1200"
+                  >
+                  <news-author v-bind="author" />
+                  </v-card>
+                </v-container>
+              </router-link>
+              <v-divider>
+              </v-divider>
+
+              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
+                <v-container class="py-2 pl-0 pr-0">
+                  <v-card
+                    elevation="0"
+                    class="mx-auto"
+                    max-width="1200"
+                  >
+                      <news-author v-bind="author" />
+                  </v-card>
+                </v-container>
+              
+              </router-link>
+              <v-divider>
+              </v-divider>
+
+
+
+              <router-link :to="{name: 'Profile'}" class="text-decoration-none">
+                <v-container class="py-2 pl-0 pr-0">
+                  <v-card
+                    elevation="0"
+                    class="mx-auto"
+                    max-width="1200"
+                  >
+                    <v-container class="py-2 pl-5">
+                      <news-author v-bind="author" />
+                    </v-container>
+                  </v-card>
+                </v-container>
+              </router-link>
+              <v-divider>
+              </v-divider>
 
             </v-col>
             <v-spacer></v-spacer>
@@ -94,7 +101,7 @@ export default {
   name: 'Findusers',
   components: {
     NewsAuthor: () => import('@/components/news/Author'),
-    },
+  },
   data: () => ({
   }),
   methods: {
